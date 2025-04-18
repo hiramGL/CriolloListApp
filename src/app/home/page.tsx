f38ffa30e287fe2710a040193fbceaa6687cdc80
PabloSerrano-jpg <pablo.serrano3@upr.edu>
2025-04-18 17:01:29 -0400

    added page redirections to student acitivities department

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -125,12 +125,12 @@ export default function Home() {
 
       {/* Additional/External Resources */}
       <section className="mt-16">
-        <h3 className="text-xl font-semibold mb-4">Additional Resources</h3>
+        <h3 className="text-xl font-semibold mb-4">Departamento de Actividades Sociales & Culturales</h3>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
           {[
-            { name: "Starting a Business", href: "/resources/starting-a-business" },
-            { name: "Marketing Tips", href: "/resources/marketing-tips" },
-            { name: "Free Certifications", href: "/resources/free-certifications" },
+            { name: "Main Page", href: "https://www.uprm.edu/p/actividades-sociales/portada" },
+            { name: "Documents and Policies", href: "https://www.uprm.edu/p/actividades-sociales/reglamentos_y_documentos" },
+            { name: "Reserve Space for Student Activities", href: "https://www.uprm.edu/p/actividades-sociales/actividades_estudiantiles_y_espacios" },
           ].map((resource) => (
             <Link key={resource.name} href={resource.href} passHref>
               <Card className="rounded-xl shadow-sm transition-colors duration-200 hover:bg-green-100 hover:text-green-800 cursor-pointer">
