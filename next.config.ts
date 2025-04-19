2d490e6b1ea41ea111d494d4f5e4af3eb46b5d74
Hiram <hiram.garcia1@upr.edu>
2025-04-18 21:43:07 -0400

    Added Ignore Lint note

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ...any other config options you have
 };
 
 export default nextConfig;
